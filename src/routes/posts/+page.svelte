6eb032c1e13fc20084f2a004f44079782f03e893
Nikhil Gupta <me@nikhgupta.com>
2024-03-08 04:01:13 +0530

    display two columns when on larger screen for post listing page

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -8,24 +8,28 @@
 	<title>Posts | Nikhil Gupta</title>
 </svelte:head>
 
-<h1>Published Posts</h1>
-<section>
-	<ul class="posts list-none">
-		{#each data.posts as post}
-			<li class="post mb-8">
-				<small>on {utils.formatDate(post.date)}</small><br />
-				<a href="/posts/{post.slug}" class="title">{post.title}</a><br />
-				<span class="description">
-					{post.description}
-				</span>
-			</li>
-		{/each}
-	</ul>
-</section>
+<div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-16">
+	<div>
+		<h1 class="">Some blog posts..</h1>
+		<section>
+			<ul class="posts list-none">
+				{#each data.posts as post}
+					<li class="post mb-4">
+						<small class="mr-3">on {utils.formatDate(post.date)}</small><br />
+						<a href="/posts/{post.slug}" class="title">{post.title}</a>
+					</li>
+				{/each}
+			</ul>
+		</section>
+	</div>
 
-<h1>Published Tools</h1>
-<ul class="posts list-none">
-	<li class="post">
-		<a href="/tools/palette-generator" class="title">OKLCH Color Palette Generator</a>
-	</li>
-</ul>
+	<div>
+		<h1 class="">and, tools I authored..</h1>
+		<ul class="posts list-none">
+			<li class="post">
+				<small class="mr-3">on Mar 8, 2024</small><br />
+				<a href="/tools/palette-generator" class="title">OKLCH Color Palette Generator</a>
+			</li>
+		</ul>
+	</div>
+</div>
