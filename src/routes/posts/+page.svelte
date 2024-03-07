9de0c95d9a59d3920d08262c3eff18c2ab8fc031
Nikhil Gupta <me@nikhgupta.com>
2024-03-08 03:21:20 +0530

    added missing pages to satisfy vercel :)

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -9,16 +9,23 @@
 </svelte:head>
 
 <h1>Published Posts</h1>
-
-<!-- Posts -->
 <section>
 	<ul class="posts list-none">
 		{#each data.posts as post}
-			<li class="post">
-				<a href="/posts/{post.slug}" class="title">{post.title}</a>
-				<p class="date">{utils.formatDate(post.date)}</p>
-				<p class="description">{post.description}</p>
+			<li class="post mb-8">
+				<small>on {utils.formatDate(post.date)}</small><br />
+				<a href="/posts/{post.slug}" class="title">{post.title}</a><br />
+				<span class="description">
+					{post.description}
+				</span>
 			</li>
 		{/each}
 	</ul>
 </section>
+
+<h1>Published Tools</h1>
+<ul class="posts list-none">
+	<li class="post">
+		<a href="/tools/palette-generator" class="title">OKLCH Color Palette Generator</a>
+	</li>
+</ul>
