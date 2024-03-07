53ceaa7badc1b85927a1098a095b3c38c0297149
Nikhil Gupta <me@nikhgupta.com>
2024-03-07 21:44:02 +0530

    trying to fix an intermittent issue in vercel deploy;

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
-import * as utils from '$lib/utils';
-import MdsvexComponent from '$lib/importers/mdsvex.svelte';
-import { loadTaxonomy } from '$lib/importers/mdsvex';
+import * as utils from './utils';
+import MdsvexComponent from './importers/mdsvex.svelte';
+import { loadTaxonomy } from './importers/mdsvex';
 
-import CodeDisplay from '$lib/components/CodeDisplay.svelte';
+import CodeDisplay from './components/codeDisplay.svelte';
 
-import type * as types from '$lib/types';
+import type * as types from './types';
 
 export { MdsvexComponent, loadTaxonomy, utils, CodeDisplay };
 export type { types };
